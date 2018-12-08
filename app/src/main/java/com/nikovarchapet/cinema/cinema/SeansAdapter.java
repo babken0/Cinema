cc05de45dc3944ea0176b83900462978424cd059
Babken <babkenboyaxchyan@gmail.com>
2018-12-08 13:37:10 +0400

    seans remove

--- app/src/main/java/com/nikovarchapet/cinema/cinema/SeansAdapter.java
+++ app/src/main/java/com/nikovarchapet/cinema/cinema/SeansAdapter.java
@@ -1,4 +1,30 @@
 package com.nikovarchapet.cinema.cinema;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
 public class SeansAdapter extends RecyclerView.Adapter<SeansAdapter.ViewHolder>{
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
