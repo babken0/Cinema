97fd55abdb7695af6d0d0ce704750d3860f5fe8c
Babken <babkenboyaxchyan@gmail.com>
2018-12-08 14:00:28 +0400

    Seans list

--- app/src/main/java/com/nikovarchapet/cinema/cinema/SeansAdapter.java
+++ app/src/main/java/com/nikovarchapet/cinema/cinema/SeansAdapter.java
@@ -2,14 +2,19 @@ package com.nikovarchapet.cinema.cinema;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SeansAdapter extends RecyclerView.Adapter<SeansAdapter.ViewHolder>{
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.recycler_view_seans, viewGroup, false);
+        return new ViewHolder(view);
     }
 
     @Override
@@ -23,8 +28,16 @@ public class SeansAdapter extends RecyclerView.Adapter<SeansAdapter.ViewHolder>{
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView hallName;
+        Button reserveBtn;
+        TextView price;
+        TextView time;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            hallName = itemView.findViewById(R.id.hallName);
+            reserveBtn = itemView.findViewById(R.id.reserveBtn);
+            price = itemView.findViewById(R.id.price);
+            time = itemView.findViewById(R.id.time);
         }
     }
 }
